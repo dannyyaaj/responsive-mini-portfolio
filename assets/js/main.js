953f9e8ae66b424fdd5246b2796b38351ed5b9eb
Danny Yang <dannyyaaj@gmail.com>
2025-06-08 00:26:29 -0500

    update projects, tweak mobile responsive styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
-const tabs = document.querySelectorAll('[data-target]');
-const tabContent = document.querySelectorAll('[data-content]');
+const tabs = document.querySelectorAll('[data-target]')
+const tabContent = document.querySelectorAll('[data-content]')
+const year = new Date().getFullYear()
+const footer = document.getElementById('footer-year')
+
+if (footer) {
+  footer.textContent = `Copyright © ${year} | Danny Yang`
+}
+
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.target);
+    const target = document.querySelector(tab.dataset.target)
     console.log(target, ' target')
 
     tabContent.forEach(content => content.classList.remove('filters__active'))
